c15d4b4b1daa5ed828a2d6aae1ef1323f5bbea14
EstefaniaRomero <estefaniaromero.torrents@gmail.com>
2022-02-09 12:45:14 +0100

    exercici 5

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,46 @@ public class Main {
 
         for (Film film : films)
         { System.out.println(film);}*/
-
-        int numberFilms = askNumberFilm();
         List<Film> films = new ArrayList<>();
+        int numberFilms = askNumberFilm();
+        introduceFilm(numberFilms,films);
+        boolean deleteFIlm = optionDeleteFilm();
+        deleteFIlm(deleteFIlm,films);
+
+
+    }
+
+    private static void deleteFIlm(boolean deleteFIlm,List <Film> films) {
+        if(deleteFIlm){
+
+            films.clear();
+            System.out.println("Les pel·lícules s'han borrat!");
+            for (Film film : films)
+            { System.out.println(film);}
+
+        }else {
+
+            for (Film film : films)
+            { System.out.println(film);}
+        }
+
+    }
+
+    private static boolean optionDeleteFilm() {
+        System.out.println("Vols eliminar totes les pel·lícules creades? S/N");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine().equalsIgnoreCase("S");
+
+
+    }
+
+    private static void introduceFilm( int numberFilms,List<Film> films) {
+
 
         for (int i = 0; i < numberFilms ; i++) {
             Film film = createFilm();
             films.add(film);
         }
-
     }
 
     private static int askNumberFilm() {
